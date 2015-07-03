9015e12f182599dceb7923edff66eba200d0c0a3
megaplanet <megaplanet.github@megaplanet.org>
2015-07-03 21:55:49 +0200

    add first version of cyberhotel, taken from djangosite

---
+++ cyberhotel/admin.py
@@ -0,0 +1,355 @@
+# coding=utf-8
+
+from django.contrib import admin
+
+from django.utils.html import format_html
+from django.core.urlresolvers import reverse      
+from import_export.admin import ImportExportModelAdmin                                                                                                                                  
+
+from cyberhotel.models import Residence,Salle,SalleDeBain,Chambre,Personne,Resident,Locataire,Location,Reduction
+from cyberhotel.models import ResidenceResource
+from django.utils.translation import ugettext as _
+
+
+from django.db.models.query import QuerySet
+
+#------- helpers --------------------------------------------------------------------
+
+
+# COULD BE USEFUL TO HAVE A LOOK AT django-admin-easy
+#  https://github.com/ebertti/django-admin-easy
+
+def htmlTooltip(o):
+  r = "<strong>%s %s</strong><br/>" % (o._meta.object_name,str(o))
+  for field in o._meta.fields:
+    name = field.name
+    type = field.get_internal_type()
+    card = "[0..1]" if field.null else "" 
+    if type in ["ForeignKey","OneToOneField"]:
+      type = type + "("+ field.related.parent_model.__name__ + ")"
+    value = str(field._get_val_from_obj(o))
+    value2 = str(o.__getattribute__(name))
+    if value2!=value:
+      value = value+' => "'+value2+'"'
+    r += "%s : %s%s = %s<br/>" % (name,type,card,value)
+  if hasattr(o._meta,"computed_fields"):
+    computed_fields = o._meta.computed_fields
+    for opname in o._meta.computed_fields:
+      value = o.__getattribute__(opname)()        
+      if isinstance(value,(tuple,list,QuerySet)) and not isinstance(value, basestring):
+        value = ", ".join([str(x) for x in value])
+      value = str(value)
+      r += "<em>%s</em>() = %s <br/>" % (opname,value)
+  return r
+        
+def objectToURL(o):
+  table=o.__class__.__name__.lower()
+  url = "/admin/cyberhotel/"+table+"/"+str(o.id)
+  tooltip = htmlTooltip(o)
+  # tooltip = "coucou"
+  return format_html("<a href='"+url+"' class='tooltip'>"+str(o)+"<span>"+tooltip+"</span></a>")
+  
+def objectsToURL(coll,sep='<br/>'):
+  return format_html(sep.join([ objectToURL(o) for o in coll ] ))
+  
+  
+class ReadOnlyAdmin(admin.ModelAdmin):
+    readonly_fields = []
+    list_editable = []
+    def get_readonly_fields(self, request, obj=None):
+      return list(self.readonly_fields) + \
+        [field.name for field in obj._meta.fields]
+    def has_add_permission(self, request, obj=None):
+      return False
+    def has_delete_permission(self, request, obj=None):
+      return False
+    def get_actions(self, request):
+      actions = super(ReadOnlyAdmin, self).get_actions(request)
+      if 'delete_selected' in actions:
+        del actions['delete_selected']
+      return actions
+      
+class ReadOnlyTabularInline(admin.TabularInline):
+    extra = 0
+    can_delete = False
+    editable_fields = []
+    readonly_fields = []
+    exclude = []
+    def get_readonly_fields(self, request, obj=None):
+      return \
+        list(self.readonly_fields) + \
+        [field.name for field in self.model._meta.fields
+          if field.name not in self.editable_fields and
+             field.name not in self.exclude]
+    def has_add_permission(self, request):
+        return False
+        
+#------- helpers --------------------------------------------------------------------
+
+
+
+        
+  
+class SalleInline(admin.TabularInline):
+  model = Salle
+  #-- edit view
+  classes = ('grp-collapse grp-open',)   # grappelli
+  fields=['numero','etage','enTravaux']
+  extra = 0
+  
+class SalleDeBainInLine(admin.StackedInline):
+  model = SalleDeBain
+  #-- edit view
+  classes = ('grp-collapse grp-closed',)
+  fields=['numero','etage','enTravaux','estSurLePallier','chambre']
+  extra = 0
+
+class ChambreInLine(admin.StackedInline):
+  model = Chambre
+  #-- edit view
+  classes = ('grp-collapse grp-closed',)          # grappelli
+  inline_classes = ('grp-collapse grp-closed',)   # grappelli # only for stackedInline
+  fields=['numero','etage','enTravaux','nbLitsSimples','nbLitsDoubles','prix','estNonFumeur']
+  extra = 0
+
+  
+
+        
+class ResidenceAdmin(admin.ModelAdmin): #ImportExportModelAdmin):
+  # display load data, dump data thanks to smuggler and grappelli adapted template
+  # http://django-grappelli.readthedocs.org/en/2.5.4/thirdparty.html
+  # the button are not display correctly with django
+  # change_list_template = 'smuggler/change_list.html'  # https://github.com/semente/django-smuggler/
+  # change_list_template = 'admin/cyberhotel/residence/change_list_bis.html'  # just a test. see templates directory
+  
+  class Media:
+     css = { "all" : ("/static/cyberhotel/residence/style.css",) }
+  
+  class Meta:
+    permissions = (
+      ("viewonly_residence", "View only residence (require change_residence")
+    )
+  resource_class = ResidenceResource   # import_export (?)
+  actions_on_top = True; actions_on_bottom = True
+  def displayChambres(self,instance): 
+    return objectsToURL(instance.chambres(),sep=',')
+  displayChambres.short_description = 'chambres'
+  displayChambres.allow_tags = True
+  def displayChambresUtiles(self,instance): 
+    return objectsToURL(instance.chambresUtiles(),sep='<br/>')
+  displayChambresUtiles.short_description = 'chambresUtiles'
+  displayChambresUtiles.allow_tags = True
+  def displaySallesDeBain(self,instance): 
+    return objectsToURL(instance.sallesDeBain(),sep=',')
+  displaySallesDeBain.short_description = 'sallesDeBain'
+  displaySallesDeBain.allow_tags = True
+  #-- edit view
+  fieldsets = [
+      ('',    
+        {'fields':      [('nom','categorie'),('etageMin','etageMax')],
+         'classes':     ['grp-collapse'],
+         'description': 'this is <a url="http://localhost:8000/admin/">some</a> text with <em>bold</em><br/>toto'}),
+      # attribute and rols can be put in any order
+      # see http://django-grappelli.readthedocs.org/en/latest/customization.html#rearrange-inlines
+      (None,  #ignored
+        {'fields':  (),   # grappelli   
+         'classes': ("placeholder _salles-2-group",),
+         'description': 'this is some text with <em>bold</em><br/>toto'}  #ignored
+         ),  
+      ('Additional information', 
+        {'fields': [('nbPlacesMax','tarifMoyen')],
+         'classes': ['grp-collapse grp-closed']}), #grappelli
+    ]
+  radio_fields = {"categorie": admin.HORIZONTAL}  # alternative to dropdown or raw_id for Choice/Dropdown
+  
+  #-- list view
+  actions_on_top = True; actions_on_bottom = False  # no effect with grappelli
+  inlines = [SalleInline,SalleDeBainInLine,ChambreInLine]
+  list_display =  ['nom','categorie','etageMin','etageMax','nbDeSalles','displayChambres','displaySallesDeBain','displayChambresUtiles']
+  list_editable = ['categorie','etageMin','etageMax']
+  
+  list_filter = ['categorie','etageMin','etageMax']
+  list_display_links =  ['nom']
+  # change_list_template = "admin/change_list_filter_sidebar.html"  # grappelli 
+  search_fields = ['nom']
+  
+  list_select_related = True   # ?
+
+
+  
+# example of a view
+class ResidencePrestige(Residence):
+    class Meta:
+        proxy = True
+
+class ReadOnlySalleInline(ReadOnlyTabularInline,SalleInline):
+  pass
+
+class ResidencePrestigeAdmin(ReadOnlyAdmin,ResidenceAdmin):
+    def queryset(self, request):
+        return self.model.objects.filter(categorie='prestige')
+    inlines = [ReadOnlySalleInline] 
+    # ,ROSalleDeBainInLine,ROChambreInLine]
+    # readonly_fields = ['nom','etageMin','etageMax','categorie','salles']
+    #-- list view
+    # list_display = ['nom','categorie']
+    # list_editable = []
+    # inlines = []
+    
+
+  
+#class EstResponsableDeInline(admin.TabularInline):
+#  model = Resident.tuteurs.through
+#  fk_name = 'from_resident'
+  
+class ResidentAdmin(admin.ModelAdmin):
+  #-- list view
+  list_display = ['nom','genre','age','estFumeur','conjoint','residence','chambreOccupee']
+  list_editable = ['genre','age','estFumeur','conjoint','chambreOccupee']
+  list_filter = ['age','genre','estFumeur','chambreOccupee']
+  c = ['nom']
+  #-- edit view
+  fields=[("nom","age","genre"),("chambreOccupee","estFumeur"),'conjoint','tuteurs']
+  # inlines = [ EstResponsableDeInline, ]
+  
+  
+  
+class SalleAdmin(admin.ModelAdmin):
+  def displayResidence(self,instance): return objectToURL(instance.residence)
+  displayResidence.short_description = 'residence'
+  displayResidence.allow_tags = True
+  
+  readonly_fields = [displayResidence]
+  #-- list view
+  list_display=['__unicode__','displayResidence','etage','enTravaux']
+  list_display_links=['__unicode__']
+  #-- edit view
+
+  
+
+class SalleDeBainAdmin(admin.ModelAdmin):
+  #-- list view
+  list_display=['__unicode__','residence','numero','etage','enTravaux','estSurLePallier','chambre']
+  list_display_links=['__unicode__']
+  #-- edit view
+
+# class DecadeBornListFilter(admin.SimpleListFilter):
+
+
+
+
+# see https://docs.djangoproject.com/en/dev/ref/contrib/admin/#modeladmin-options
+class EtageTypeListFilter(admin.SimpleListFilter):
+    title = _('type d etage')
+    parameter_name = 'etageType'
+    def lookups(self, request, model_admin):
+      qs = model_admin.get_queryset(request)
+      if qs.filter(etage__lte=1).exists():
+        yield ('rdc', _('rdc'))
+      if qs.filter(etage__gte=2).exists():
+        yield ('autre', _('autre'))
+
+    def queryset(self, request, queryset):
+        if self.value() == 'rdc':
+            return queryset.filter(etage__lte=1)
+        if self.value() == 'autre':
+            return queryset.filter(etage__gte=2)
+
+
+
+
+
+class ChambreAdmin(admin.ModelAdmin):
+  def displayResidence(self,instance): return objectToURL(instance.residence)
+  displayResidence.short_description = 'residence'
+  displayResidence.allow_tags = True
+  displayResidence.admin_order_field = 'residence'
+  def displayOccupants(self,instance): return objectsToURL(instance._occupants.all())
+  displayOccupants.short_description = 'occupants'
+  displayOccupants.allow_tags = True
+  # always put operation and display operation into readonly_fields:
+  # this makes it possible to put them in list_display (list view) and fields view
+  readonly_fields = ['nbDePlaces','occupants','occupantsList','displayResidence','displayOccupants']
+  #-- list view
+  list_display=['__unicode__','displayResidence','etage','enTravaux','nbLitsSimples','nbLitsDoubles','prix','estNonFumeur','nbDeOccupants','nbDePlaces','displayOccupants']
+  list_display_links=['__unicode__']
+  # change_list_template = "admin/change_list_filter_sidebar.html"  # grappelli 
+  list_filter = ['residence','residence__categorie',EtageTypeListFilter,'etage','enTravaux','nbLitsSimples','nbLitsDoubles','estNonFumeur']
+  search_fields = ['prix','residence__nom']
+  ordering=['residence','numero']
+  list_per_page=10
+  #-- edit view
+  fields = ['residence',('numero','etage'),'enTravaux',('nbLitsSimples','nbLitsDoubles'),'estNonFumeur','prix','nbDePlaces','displayOccupants']
+  
+
+class LocataireAdmin(admin.ModelAdmin):
+  pass
+  
+class ReductionInline(admin.TabularInline):
+  model = Reduction
+  extra = 0  
+  
+class LocationAdmin(admin.ModelAdmin):
+  list_display = ['locataire','chambreLouee','dateDepart','dateFin']
+  inlines = [ReductionInline]
+
+
+
+
+admin.site.register(Residence,ResidenceAdmin)
+admin.site.register(Salle,SalleAdmin)  
+admin.site.register(SalleDeBain,SalleDeBainAdmin)  
+admin.site.register(Chambre,ChambreAdmin)  
+admin.site.register(Resident,ResidentAdmin)
+admin.site.register(Locataire,LocataireAdmin)
+admin.site.register(Location,LocationAdmin)
+admin.site.register(ResidencePrestige, ResidencePrestigeAdmin)
+
+# ===== introspection of an django object ========================
+# http://stackoverflow.com/questions/2233883/get-all-related-django-model-objects
+
+# ======  add a user to the creator of an object ========
+# # app/models.py
+
+# from https://code.djangoproject.com/wiki/CookBookNewformsAdminAndUser
+
+# from django.db import models
+# from django.contrib.auth.models import User
+
+# class Post(models.Model):
+    # user = models.ForeignKey(User)
+    # content = models.TextField()
+
+# class Comment(models.Model):
+    # post = models.ForeignKey(Post)
+    # user = models.ForeignKey(User)
+    # content = models.TextField()
+
+# # app/admin.py
+
+# from app.models import Post, Comment
+# from django.contrib import admin
+
+# class CommentInline(admin.TabularInline):
+    # model = Comment
+    # fields = ('content',)
+
+# class PostAdmin(admin.ModelAdmin):
+
+    # fields= ('content',)
+    # inlines = [CommentInline]
+
+    # def save_model(self, request, obj, form, change): 
+        # obj.user = request.user
+        # obj.save()
+
+    # def save_formset(self, request, form, formset, change): 
+        # if formset.model == Comment:
+            # instances = formset.save(commit=False)
+            # for instance in instances:
+                # instance.user = request.user
+                # instance.save()
+        # else:
+            # formset.save()
+
+# admin.site.register(Post, PostAdmin)
---
+++ cyberhotel/models.py
@@ -0,0 +1,150 @@
+from import_export import resources
+import polymorphic
+from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator,MaxValueValidator
+from django.utils.translation import ugettext as _
+
+
+import django.db.models.options as options
+options.DEFAULT_NAMES = options.DEFAULT_NAMES + ('computed_fields',)
+
+CATEGORIE = (
+    ('economique', 'economique'),
+    ('standard', 'standard'),
+    ('premium', 'premium'),
+    ('prestige', 'prestige'),
+)
+
+class Residence(models.Model):
+  class Meta:
+    computed_fields = ['chambres','nbDeChambres','chambresUtiles','sallesDeBain','salles','nbDeSalles']
+  nom         = models.CharField(max_length=60)
+  etageMin	  = models.IntegerField(default=0)
+  etageMax    = models.IntegerField()
+  categorie   = models.CharField(max_length=10, choices=CATEGORIE)
+  nbPlacesMax = models.IntegerField(blank=True,null=True)   # TODO
+  tarifMoyen  = models.FloatField(blank=True,null=True)     # TODO 
+  def chambres(self):
+    return self.salles().instance_of(Chambre)
+  def nbDeChambres(self):
+    return len(self.chambres())
+  def chambresUtiles(self):
+    return  [ s for s in self.chambres() if not s.enTravaux ]
+  def sallesDeBain(self):
+    return self.salles().instance_of(SalleDeBain)
+  def salles(self):
+    return self._salles.all()
+  def nbDeSalles(self):
+    return len(self.salles())
+  def __unicode__(self):
+    return self.nom 
+  # for validating entity constraint
+  def validateOrdreEtages(self):
+    if not (self.etageMin <= self.etageMax):
+       raise ValidationError(_(u"etage minimum est plus haut que l'etage max"),code="ordreEtages")
+  def clean(self):
+    self.validateOrdreEtages() 
+  
+class Salle(polymorphic.PolymorphicModel):
+  residence   = models.ForeignKey(Residence,related_name="_salles")
+  etage	      = models.IntegerField()
+  enTravaux	  = models.BooleanField()
+  numero      = models.IntegerField()
+  def __unicode__(self):
+    return str(self.numero)
+  def validateEtageEntreMinEtMax(self):
+    if not(self.residence.etageMin <= self.etage and self.etage<=self.residence.etageMax):
+      raise ValidationError(
+        _(u"l'etage doit etre entre %i et %i, les etages de la residences") % (self.residence.etageMin,self.residence.etageMax),code="validateEtageEntreMinEtMax")
+  def clean(self):
+    self.validateEtageEntreMinEtMax()
+    
+class SalleDeBain(Salle):
+  estSurLePallier = models.BooleanField()
+  chambre         = models.ForeignKey('Chambre',related_name="_sallesDeBains",blank=True,null=True)
+  def __unicode__(self):
+    return str(self.numero)
+    
+class Chambre(Salle):
+  class Meta:
+    computed_fields = ['nbDePlaces','occupants','nbDeOccupants','occupantsList']
+  nbLitsSimples = models.IntegerField(default=1,verbose_name='nb de lits simples')
+  nbLitsDoubles	= models.IntegerField(default=0)
+  prix	        = models.FloatField(blank=True,null=True)
+  estNonFumeur  = models.BooleanField() 
+  def nbDePlaces(self):
+    return self.nbLitsSimples+self.nbLitsDoubles*2
+  def occupants(self):
+    return list(self._occupants.all())
+  def nbDeOccupants(self):
+    return len(self.occupants())
+  def occupantsList(self):
+    return ",".join(str(o) for o in self.occupants())
+  def __unicode__(self):
+    return str(self.numero)
+
+    
+GENRE = {
+    ('homme', 'homme'),
+    ('femme', 'femme'),
+}
+
+class Personne(models.Model):
+  class Meta:
+    abstract = True
+  nom    = models.CharField(max_length=40)
+  age    = models.IntegerField()
+  genre  = models.CharField(max_length=5, choices=GENRE)
+  
+class Resident(Personne):
+  estFumeur      = models.NullBooleanField()
+  chambreOccupee = models.ForeignKey(Chambre,related_name="_occupants")
+  conjoint       = models.OneToOneField("self",related_name="+",blank=True,null=True)
+  tuteurs        = models.ManyToManyField("self",symmetrical=False,related_name='tutores',blank=True,null=True)
+  def __unicode__(self):
+    return self.nom
+  def residence(self):
+    return self.chambreOccupee.residence
+    
+class ResidenceResource(resources.ModelResource):
+    class Meta:
+        model = Residence
+    
+class Locataire(Resident):
+  def prixPaye(self):
+    return 0  # TODO
+  def __unicode__(self):
+    return self.nom
+    
+class Location(models.Model):
+  chambreLouee    = models.ForeignKey(Chambre,related_name="_locations")   # TODO plural +s -e  to be resolved
+  locataire       = models.ForeignKey(Locataire,related_name="_locations")
+  dateDepart      = models.DateField()
+  dateFin         = models.DateField()
+  def reduction(self):
+    return 0  # TODO
+  def prix(self):
+    return 0  # TODO
+  def __unicode__(self):
+    return str(self.chambreLouee.numero)+"/"+self.locataire.nom
+
+
+
+        
+class Reduction(models.Model):
+  location   = models.ForeignKey(Location,related_name="_reductions")
+  def validate_taux(taux):
+    if  not (0<=taux and taux<=100):
+        raise ValidationError(_(u"%s is not a percentage")% taux,code="tauxPercentage")  
+  taux     = models.IntegerField(validators=[validate_taux])
+  #  taux  = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(100)])
+  label      = models.CharField(max_length=10)
+
+  
+    
+#  def user_link(self):
+#    return '<a href="%s">%s</a>' % (reverse("admin:auth_user_change", args=(self.user.id,)) , escape(self.user))
+#
+#  user_link.allow_tags = True
+#  user_link.short_description = "User" 
