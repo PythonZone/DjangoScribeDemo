a8f715571f748f41428486fabced86c0148e27f4
megaplanet <megaplanet.github@megaplanet.org>
2015-07-13 21:50:34 +0200

    misc

--- cyberhotel/models/__init__.py
+++ cyberhotel/models/__init__.py
@@ -3,7 +3,8 @@
 
 from __future__ import absolute_import, print_function
 
-from .buildings import Category, Residence, Room, Bedroom, Bathroom
+from .buildings import \
+    Category, Residence, ResidencePrestige, Room, Bedroom, Bathroom
 from .rents import Gender, Person, Resident, Tenant
 from .rates import Rent, Discount, validateDomainPercentage
 
